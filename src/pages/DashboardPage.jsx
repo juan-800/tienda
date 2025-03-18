8937d2647771eb0b4cc3202d8e5cef4b6d104958
juan-800 <juanes-800@hotmail.com>
2025-03-18 18:16:53 -0500

    realizando pages dashboardPage

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import { Header } from "../components/Header";
+import { ProductList } from "../components/ProductList";
+import { useState } from "react";
 
 export const DashboardPage = () => {
+  const [allProducts, setAllProducts] = useState([]);
+  const [countProducts, setCountProducts] = useState(0);
+  const [total, setTotal] = useState(0);
+
   return (
-    <h1>hola juan</h1>
-  )
-}
+    <>
+      <Header
+        allProducts={allProducts}
+        setAllProducts={setAllProducts}
+        total={total}
+        countProducts={countProducts}
+        setCountProducts={setCountProducts}
+        setTotal={setTotal}
+      />
+      <ProductList
+        allProducts={allProducts}
+        setAllProducts={setAllProducts}
+        countProducts={countProducts}
+        setCountProducts={setCountProducts}
+        total={total}
+        setTotal={setTotal}
+      />
+    </>
+  );
+};
+
+
